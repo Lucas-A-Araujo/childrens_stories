3d2bf31b8b74592c285a19c17497c60304ed5244
Lucas Alves <lucas.al.araujo10@gmail.com>
2024-05-29 09:49:13 -0300

    chore: create header token

--- lib/src/networks/interceptors_app.dart
+++ lib/src/networks/interceptors_app.dart
@@ -1,3 +1,24 @@
+import 'dart:convert';
+
+import 'package:childrens_stories/src/networks/constants.dart';
+import 'package:childrens_stories/src/packages/local_storage_packages.dart';
+import 'package:childrens_stories/src/utils/key_names_utils.dart';
+
 class InterceptorsApp {
-  headerToken() async {}
+  final LocalStoragePackages _local = LocalStoragePackages();
+  headerToken() async {
+    String? tokenString = await _local.readLocal(key: KeyNamesUtils.token);
+
+    Map<String, String> header = {};
+
+    if (tokenString != '') {
+      String? token = json.decode(tokenString!);
+
+      header['Authorization'] = (token != null ? 'Bearer $token' : null)!;
+      header['Content-Type'] = 'application/json';
+      Constants.headerConstAPI = header;
+    }
+
+    return header;
+  }
 }
